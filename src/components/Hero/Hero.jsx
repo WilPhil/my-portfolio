be25b4bd5881c92aa8627b1c1eeb08c9817e50e3
Wilsent Philip <wphilip465@gmail.com>
2026-02-11 19:48:05 +0700

    add: Hero Section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,56 @@
+import GithubIcon from "../../assets/GitHub_Invertocat_White.svg";
+import LinkedinIcon from "../../assets/InBug-White.png";
+import ProfileIcon from "../../assets/Profile.jpg";
+
 const Hero = () => {
   return (
     <section id="home" className="min-h-screen flex items-center">
       <div className="container mx-auto px-6">
         <div className="max-w-4xl mx-auto flex">
-          <p>
-            Hello, I'm a passionate Full-Stack Developer. Learning to thrive
-            into the web development world
-          </p>
-          <img
-            // 4. Then change the src below from the placeholder to {myImage}
-            src="https://placehold.co/192x192/47d1d1/ffffff?text=You"
-            alt="Your Name"
-            className="w-36 h-36 md:w-48 md:h-48 rounded-full object-cover mx-auto mb-8 border-4 border-cyan-400 shadow-lg"
-          />
+          <div className="w-[80%] flex flex-col pr-6 gap-4">
+            <div className="flex justify-end gap-4 pr-4">
+              <a
+                href="https://github.com/WilPhil"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-cyan-400 hover:text-cyan-300 transition duration-300 ease-in-out"
+              >
+                <img
+                  src={GithubIcon}
+                  alt="Github"
+                  className="w-6 h-6 hover:opacity-70 ease-in-out transition duration-300"
+                />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/wilsent-philip-33bbb0225/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-cyan-400 hover:text-cyan-300 transition duration-300 ease-in-out"
+              >
+                <img
+                  src={LinkedinIcon}
+                  alt="Linkedin"
+                  className="w-6 h-6 hover:opacity-70 ease-in-out transition duration-300"
+                />
+              </a>
+            </div>
+            <div className="flex flex-col items-end gap-4">
+              <span className="text-5xl md:text-7xl font-bold tracking-tighter text-white">
+                Wilsent Philip Lo
+              </span>
+              <span className="text-sm md:text-base font-medium text-purple-500 uppercase tracking-[0.2em]">
+                Web Developer
+              </span>
+            </div>
+          </div>
+          <div className="w-[20%] flex items-center">
+            <img
+              src={ProfileIcon}
+              alt="Wilsent Philip Lo"
+              className="w-40 h-40 rounded-full object-cover mx-auto mb-8"
+              style={{ imageRendering: "auto" }}
+            />
+          </div>
         </div>
       </div>
     </section>
