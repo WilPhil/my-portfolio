26d691351859688ff996aa251793542be0b96be1
Wilsent Philip <wphilip465@gmail.com>
2026-02-11 19:49:45 +0700

    update: npm packages and apply changes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-import Navbar from './components/Navbar/Navbar.jsx';
-import Hero from './components/Hero/Hero.jsx';
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Hero from "./components/Hero/Hero.jsx";
+import About from "./components/About/About.jsx";
+import Expertise from "./components/Expertise/Expertise.jsx";
+import Experience from "./components/Experience/Experience.jsx";
+import Projects from "./components/Projects/Projects.jsx";
 
 function App() {
   return (
@@ -7,6 +11,28 @@ function App() {
       <Navbar />
       <main>
         <Hero />
+        <About />
+        <div className="w-full ">
+          <p className="italic text-lg text-center text-[#e0e0e0] leading-relaxed">
+            "Building the bridge between intelligent systems & seamless users
+            experience."
+          </p>
+        </div>
+        <Expertise />
+        <div className="w-full ">
+          <p className="italic text-lg text-center text-[#e0e0e0] leading-relaxed">
+            "Knowledge is the foundation; experience is the architecture built
+            upon it."
+          </p>
+        </div>
+        <Experience />
+        <div className="w-full ">
+          <p className="italic text-lg text-center text-[#e0e0e0] leading-relaxed">
+            "Where professional foundations meet personal exploration, turning
+            lines of code into living systems."
+          </p>
+        </div>
+        <Projects />
       </main>
     </div>
   );
