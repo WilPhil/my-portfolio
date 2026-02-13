ce802d40ad2d76bc1271bf770c4f8ad09dc1aceb
Wilsent Philip <wphilip465@gmail.com>
2026-02-13 20:27:34 +0700

    add: purple ambience animation and email hyperlink

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,19 @@
 import GithubIcon from "../../assets/GitHub_Invertocat_White.svg";
 import LinkedinIcon from "../../assets/InBug-White.png";
 import ProfileIcon from "../../assets/Profile.jpg";
+import Envelope from "../../assets/envelope-solid.svg";
 
 const Hero = () => {
   return (
-    <section id="home" className="min-h-screen flex items-center">
-      <div className="container mx-auto px-6">
+    <section
+      id="home"
+      className="relative min-h-screen flex items-center overflow-hidden bg-[#121212]"
+    >
+      <div className="absolute top-1/4 left-1/4 -translate-x-1/2 -translate-y-1/2 w-[333px] h-[333px] bg-purple-600/30 rounded-full blur-[120px] pointer-events-none animate-pulse [animation-duration:4s]"></div>
+
+      <div className="absolute -bottom-14 right-1 -translate-x-1/2 -translate-y-1/2 w-[333px] h-[333px] bg-purple-600/30 rounded-full blur-[120px] pointer-events-none animate-pulse [animation-duration:4s]"></div>
+
+      <div className="container mx-auto px-6 relative z-10">
         <div className="max-w-4xl mx-auto flex">
           <div className="w-[80%] flex flex-col pr-6 gap-4">
             <div className="flex justify-end gap-4 pr-4">
@@ -30,6 +38,16 @@ const Hero = () => {
                 <img
                   src={LinkedinIcon}
                   alt="Linkedin"
+                  className="w-6 h-6 hover:opacity-70 ease-in-out transition duration-300"
+                />
+              </a>
+              <a
+                href="mailto:wphilip465@gmail.com"
+                className="text-cyan-400 hover:text-cyan-300 transition duration-300 ease-in-out"
+              >
+                <img
+                  src={Envelope}
+                  alt="Email"
                   className="w-6 h-6 hover:opacity-70 ease-in-out transition duration-300"
                 />
               </a>
